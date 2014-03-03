6d53d20a72a58d65b59b95c0a130d0b1bd19641a
chirag sanghvi <csanghvi@appacitive.com>
2014-03-03 14:48:57 +0530

    changed apiversion

--- config/contextConfig.js
+++ config/contextConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
 	//baseUrl: 'http://stage-apis.appacitive.com/v0.9/core/',
-	baseUrl: 'http://apis.appacitive.com/',
+	baseUrl: 'http://apis.appacitive.com/v1.0/',
 	path:'/' ,
 	loaderIO: "loaderio-02c74db7ffc3daa187c9d2ec9ef620d8"
 };
